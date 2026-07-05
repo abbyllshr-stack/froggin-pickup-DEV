c62c1cd3ecf862ab211c3c887165adc19d11f567
abbyllshr-stack <abby.ll.sh.r@gmail.com>
2026-07-05 12:48:51 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+// ==========================================
+// FROGGIN PICKUP v1.1
+// CONFIGURACIÓN
+// ==========================================
+
+const CONFIG = {
+
+    API_URL: "https://script.google.com/macros/s/AKfycbxnfKzj1Fdd31lNaRgEveoz_Fk0z28tB61LhTRry6pFdZTfSZb3HXtzV0YF6opYdjoC/exec",
+
+    REFRESH_TIME: 2000,
+
+    RESULT_TIME: 2000,
+
+    CAMERA: {
+
+        fps:5,
+
+        qrbox:180,
+
+        facingMode:"environment"
+
+    }
+
+};

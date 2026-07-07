6e108d55e728ab88457fab43ffcbfa12a5ea7a30
abbyllshr-stack <abby.ll.sh.r@gmail.com>
2026-07-07 11:08:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -175,13 +175,8 @@ function mostrarPantallaReposicion(datos){
 
             <option ${datos.teacher=="Chantal"?"selected":""}>Chantal</option>
 
-            <option ${datos.teacher=="Abril"?"selected":""}>Mariana</option>
-          
-            <option ${datos.teacher=="Edwin"?"selected":""}>Angel</option>
+            <option ${datos.teacher=="Mariana"?"selected":""}>Mariana</option>
 
-            <option ${datos.teacher=="Yessica"?"selected":""}>Chantal</option>
-
-            <option ${datos.teacher=="Ana"?"selected":""}>Mariana</option>
         </select>
 
         <br><br>
@@ -207,11 +202,10 @@ function mostrarPantallaReposicion(datos){
 
 async function enviarSolicitudAutomatica(datos){
 
-    const url =
-        API_URL +
-        "?action=enviar" +
-        "&id=" + encodeURIComponent(alumnoActual) +
-        "&teacher=" + encodeURIComponent(datos.teacher);
+const url =
+    API_URL +
+    "?action=enviar" +
+    "&id=" + encodeURIComponent(alumnoActual);
 
     try{
 
